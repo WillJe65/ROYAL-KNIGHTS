d31b826603388e72eb4497285e8097357dce118d
Ghostie <dzakipra87gt@gmail.com>
2025-10-28 23:02:16 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My React Task Manager</h1>
+        <p>Selamat datang di aplikasi Task Manager!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
